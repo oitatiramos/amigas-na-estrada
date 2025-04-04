6936cf95842100119d14dd2b12bba139d7ad2df9
Tati Ramos <contatotatiramos@gmail.com>
2025-04-03 22:47:42 -0300

    first commit

---
+++ src/main/java/br/com/amigas_na_estrada/AmigasNaEstradaApplication.java
@@ -0,0 +1,13 @@
+package br.com.amigas_na_estrada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigasNaEstradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigasNaEstradaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/amigas_na_estrada/AmigasNaEstradaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amigas_na_estrada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigasNaEstradaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
